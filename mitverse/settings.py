584a85c2c08b70a25f17ebb267c355e25ee9e546
CargorsAries <116709439+CargorsAries@users.noreply.github.com>
2023-07-15 19:24:11 +0600

    settings has been updated again

--- mitverse/settings.py
+++ mitverse/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+PROJECT_DIR = os.path.dirname(Path(__file__).resolve().parent.parent)
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 
