4c77bbe3361a7a1be12405a3a5f2725e728b69c9
CargorsAries <116709439+CargorsAries@users.noreply.github.com>
2023-07-15 14:37:15 +0600

    updates

--- mitverse/urls.py
+++ mitverse/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    # path('', TemplateView.as_view(template_name='index.html')),
     path('webdrive/', include("webdrive.urls")),
     path('puthibidda/', include("puthibidda.urls")),
     path('bankjobbd/', include("bankjobbd.urls")),
